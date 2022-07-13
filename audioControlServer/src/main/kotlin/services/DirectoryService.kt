90f1a8b65ffecfbe412b995216a8cb60c857bfd7
Sunken Ahn <devsanso@outlook.com>
2022-07-14 01:36:58 +0900

    디렉토리 서비스, 파일 입출력 역할을 할 코드로 작성 예정

---
+++ audioControlServer/src/main/kotlin/services/DirectoryService.kt
@@ -0,0 +1,32 @@
+package com.github.DevSanso.audioControlServer.services
+
+import com.github.DevSanso.audioControlServer.model.DirectoryModel
+import com.github.DevSanso.audioControlServer.repository.FileMetaRepository
+import com.github.DevSanso.audioControlServer.extension.dtfp.FileMetaEntityToFileDataModel
+import com.github.DevSanso.audioControlServer.extension.dtfp.FileMetaEntityToFileDataModel.toFileDataModel
+import org.apache.commons.io.FilenameUtils
+
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import javax.transaction.Transactional
+
+
+@Service
+class DirectoryService @Autowired constructor(private val directoryModel: DirectoryModel,
+                                              private val fileMetaRepository: FileMetaRepository){
+
+
+    fun fileMetaDataList() =  fileMetaRepository.findAll().map {it.toFileDataModel()}
+
+
+    @Transactional
+    fun uploadFile(file : MultipartFile) : Result<Unit> {
+
+
+
+
+        return Result.success(Unit)
+    }
+}
