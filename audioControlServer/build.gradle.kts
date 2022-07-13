c8e55e391d5e5a2a878ddb7df454b344797cccf3
Sunken Ahn <devsanso@outlook.com>
2022-07-14 01:34:26 +0900

    noarg 플러그인 추가

--- audioControlServer/build.gradle.kts
+++ audioControlServer/build.gradle.kts
@@ -1,8 +1,21 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+
+
+
+buildscript {
+	repositories {
+		mavenCentral()
+	}
+	dependencies {
+		classpath("org.jetbrains.kotlin:kotlin-noarg:1.6.21")
+	}
+}
+
 plugins {
 	id("org.springframework.boot") version "2.7.1"
 	id("io.spring.dependency-management") version "1.0.12.RELEASE"
+	id("org.jetbrains.kotlin.plugin.noarg") version "1.6.21"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 }
@@ -11,6 +24,8 @@ group = "com.github.DevSanso"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
+
+
 repositories {
 	mavenCentral()
 	maven { url = uri("https://repo.spring.io/milestone") }
@@ -18,14 +33,15 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-graphql")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.apache.tika:tika-core:2.4.1")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.1")
+	runtimeOnly("org.mariadb.jdbc:mariadb-java-client:3.0.6")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework:spring-webflux")
-	testImplementation("org.springframework.graphql:spring-graphql-test")
 }
 
 tasks.withType<KotlinCompile> {
@@ -37,4 +53,13 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+
+allOpen {
+	annotation("javax.persistence.Entity") // @Entity가 붙은 클래스에 한해서만 all open 플러그인을 적용
+}
+
+noArg {
+	annotation("javax.persistence.Entity")
 }
