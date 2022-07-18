bbe2f0c56ddc29214a77e0cce112f1e111225efc
Sunken Ahn <devsanso@outlook.com>
2022-07-18 12:41:38 +0900

    서비스 인터페이스, 구현 클래스 분리

--- audioControlServer/src/main/kotlin/services/DirectoryService.kt
+++ audioControlServer/src/main/kotlin/services/DirectoryService.kt
@@ -1,38 +1,15 @@
 package com.github.DevSanso.audioControlServer.services
 
-import com.github.DevSanso.audioControlServer.model.DirectoryModel
-import com.github.DevSanso.audioControlServer.repository.FileMetaRepository
-import com.github.DevSanso.audioControlServer.extension.dtfp.MultipartFileToFileMetaEntity.toFileMetaEntity
-import com.github.DevSanso.audioControlServer.extension.dtfp.FileMetaEntityToFileDataModel.toFileDataModel
+
 import com.github.DevSanso.audioControlServer.model.FileMetaDataModel
-
-import org.apache.commons.io.FilenameUtils
-
-
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 import javax.transaction.Transactional
 
-
-@Service
-class DirectoryService @Autowired constructor(private val directoryModel: DirectoryModel,
-                                              private val fileMetaRepository: FileMetaRepository){
-
-
-    fun fileMetaDataList() =  fileMetaRepository.findAllAndSortedTitle().map {it.toFileDataModel()}
-
-
+interface DirectoryService{
+    fun fileMetaDataList() : List<FileMetaDataModel>
     @Transactional
-    fun uploadFile(file : MultipartFile) {
-        val entity = file.toFileMetaEntity()
-        fileMetaRepository.save(entity)
-        directoryModel.upload(file)
-    }
+    fun uploadFile(file : MultipartFile)
     @Transactional
-    fun deleteFile(fileName : String) {
-        fileMetaRepository.deleteAllById(listOf(fileName))
-        directoryModel.delete(fileName)
-    }
+    fun deleteFile(fileName : String)
 
 }
---
+++ audioControlServer/src/main/kotlin/services/DirectoryServiceImpl.kt
@@ -0,0 +1,31 @@
+package com.github.DevSanso.audioControlServer.services
+
+import com.github.DevSanso.audioControlServer.extension.dtfp.FileMetaEntityToFileDataModel.toFileDataModel
+import com.github.DevSanso.audioControlServer.extension.dtfp.MultipartFileToFileMetaEntity.toFileMetaEntity
+import com.github.DevSanso.audioControlServer.model.DirectoryModel
+import com.github.DevSanso.audioControlServer.repository.FileMetaRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import javax.transaction.Transactional
+
+
+@Service("DirectoryService")
+class DirectoryServiceImpl @Autowired constructor(private val directoryModel: DirectoryModel,
+                                                 private val fileMetaRepository: FileMetaRepository) : DirectoryService{
+
+    override fun fileMetaDataList() =  fileMetaRepository.findAllAndSortedTitle().map {it.toFileDataModel()}
+
+    @Transactional
+    override fun uploadFile(file : MultipartFile) {
+        val entity = file.toFileMetaEntity()
+        fileMetaRepository.save(entity)
+        directoryModel.upload(file)
+    }
+    @Transactional
+    override fun deleteFile(fileName : String) {
+        fileMetaRepository.deleteAllById(listOf(fileName))
+        directoryModel.delete(fileName)
+    }
+
+}
